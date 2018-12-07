fda2524645263090e9443435e118eae46e51d771
Stephen Wolff <stephen@maxgatedigital.com>
2018-12-07 22:26:27 +0000

    Better stop words and logging

--- url_wordcloud/handlers/base.py
+++ url_wordcloud/handlers/base.py
@@ -23,18 +23,9 @@ from url_wordcloud.models import Word
 # could use nltk with more time
 from url_wordcloud.utils import salted_hash, aes_encrypted
 
-STOP_WORDS_RE = r"^(i|me|my|myself|we|us|our|ours|ourselves|you|your|yours|yourself|yourselves|he|him|his|himself|" \
-                r"she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves|what|which|who|whom|whose|" \
-                r"this|that|these|those|am|is|are|was|were|be|been|being|have|has|had|having|do|does|did|doing|will|" \
-                r"would|should|can|could|ought|i'm|you're|he's|she's|it's|we're|they're|i've|you've|we've|they've|" \
-                r"i'd|you'd|he'd|she'd|we'd|they'd|i'll|you'll|he'll|she'll|we'll|they'll|isn't|aren't|wasn't|" \
-                r"weren't|hasn't|haven't|hadn't|doesn't|don't|didn't|won't|wouldn't|shan't|shouldn't|can't|cannot|" \
-                r"couldn't|mustn't|let's|that's|who's|what's|here's|there's|when's|where's|why's|how's|a|an|the|and|" \
-                r"but|if|or|because|as|until|while|of|at|by|for|with|about|against|between|into|through|during|" \
-                r"before|after|above|below|to|from|up|upon|down|in|out|on|off|over|under|again|further|then|once|" \
-                r"here|there|when|where|why|how|all|any|both|each|few|more|most|other|some|such|no|nor|not|only|own|" \
-                r"same|so|than|too|very|say|says|said|shall)$"
-
+STOP_WORDS_PIPED = "i|me|my|myself|we|us|our|ours|ourselves|you|your|yours|yourself|yourselves|he|him|his|himself|she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves|what|which|who|whom|whose|this|that|these|those|am|is|are|was|were|be|been|being|have|has|had|having|do|does|did|doing|will|would|should|can|could|ought|i'm|you're|he's|she's|it's|we're|they're|i've|you've|we've|they've|i'd|you'd|he'd|she'd|we'd|they'd|i'll|you'll|he'll|she'll|we'll|they'll|isn't|aren't|wasn't|weren't|hasn't|haven't|hadn't|doesn't|don't|didn't|won't|wouldn't|shan't|shouldn't|can't|cannot|couldn't|mustn't|let's|that's|who's|what's|here's|there's|when's|where's|why's|how's|a|an|the|and|but|if|or|because|as|until|while|of|at|by|for|with|about|against|between|into|through|during|before|after|above|below|to|from|up|upon|down|in|out|on|off|over|under|again|further|then|once|here|there|when|where|why|how|all|any|both|each|few|more|most|other|some|such|no|nor|not|only|own|same|so|than|too|very|say|says|said|shall"
+STOP_WORDS = STOP_WORDS_PIPED.split('|')
+STOP_WORDS_RE = re.compile(r'\b(' + r'|'.join(STOP_WORDS) + r')\b\s*')
 
 class MainHandler(tornado.web.RequestHandler):
 
@@ -69,6 +60,7 @@ class AnalyseURLHandler(SessionMixin, tornado.websocket.WebSocketHandler):
 
         # TODO: validate url, ie protocol etc
         url_for_wordcloud = parsed["url_for_wordcloud"]
+        logging.info("url_for_wordcloud %r", url_for_wordcloud)
 
         # retrieve the URL asynchronously
         # include a User-Agent, as some servers dislike bots
@@ -77,12 +69,19 @@ class AnalyseURLHandler(SessionMixin, tornado.websocket.WebSocketHandler):
 
         # get words from response body
         soup = BeautifulSoup(response.body, 'lxml')
+        [s.extract() for s in soup('script')]
         word_frequency_dict = {}
+
+        for script in soup(["script", "style", "iframe"]):
+            script.decompose()  # rip it out
+
+        # get text
 
         if soup:
             # get the title, and all paragraphs (ie ignore script tags etc
-            text_tags = soup.body.find_all(['title', 'p'])
-            words = ' '.join ([tag.string for tag in text_tags if tag.string is not None]).lower()
+            words = soup.get_text()
+            #text_tags = soup.find_all(['div'])
+            #words = ' '.join ([tag.string for tag in text_tags if tag.string is not None]).lower()
 
             # remove punctuation
             words = re.sub(r'[^\w\s]', '', words)
@@ -90,8 +89,13 @@ class AnalyseURLHandler(SessionMixin, tornado.websocket.WebSocketHandler):
             # remove line breaks
             words = re.sub(r"\n|\r", " ", words)
 
+            # remove double spaces
+            words = re.sub(' +', ' ', words)
+
             # remove stop words (leaving nouns and verbs)
             words = re.sub(STOP_WORDS_RE, '', words)
+
+            logging.info("words %r", words)
 
             # TODO alternative if time, and ntlk stop words available
             # remove articles and prepositions [stop words]
@@ -107,6 +111,7 @@ class AnalyseURLHandler(SessionMixin, tornado.websocket.WebSocketHandler):
 
             # store the top 100
             top_hundred = word_frequency_dict.most_common(100)
+            logging.info("top_hundred %r", top_hundred)
 
             with self.make_session() as session:
                 words = []
@@ -118,7 +123,7 @@ class AnalyseURLHandler(SessionMixin, tornado.websocket.WebSocketHandler):
                     encrypted_word = aes_encrypted(word)
 
                     # get word from db in non-blocking fashion
-                    db_word = session.query(Word).filter_by(pk=pk).first()
+                    db_word = await as_future(session.query(Word).filter_by(pk=pk).first)
 
                     if db_word is not None:
                         # update
@@ -136,10 +141,18 @@ class AnalyseURLHandler(SessionMixin, tornado.websocket.WebSocketHandler):
 
                     # session.merge(new_word, load=True)
 
+                # check whether this makes any db calls?
+                # i think it just updates the sqlalchemy session
                 session.add_all(words)
+
+                # not sure whether i need to make this async, but it should make db calls which are blocking
+                # await as_future(session.commit())
                 session.commit()
 
-            self.write_message(json.dumps(word_frequency_dict))
+            word_frequency_json = json.dumps(word_frequency_dict)
+            logging.info("top_hundred %r", top_hundred)
+
+            self.write_message(word_frequency_json)
         else:
             self.write_error("No words found")
 
