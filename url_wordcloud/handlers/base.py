9116fb283bac6e76cfdd1a21f10dd00b863a7fbf
Stephen Wolff <stephen@maxgatedigital.com>
2018-12-07 21:09:01 +0000

    Save (insert of update) words to database, stub out encryption functions

--- url_wordcloud/handlers/base.py
+++ url_wordcloud/handlers/base.py
@@ -19,6 +19,22 @@ from tornado_sqlalchemy import SessionMixin, as_future
 # from nltk.corpus import stopwords
 from url_wordcloud.models import Word
 
+# use regular expression to remove common words
+# could use nltk with more time
+from url_wordcloud.utils import salted_hash, aes_encrypted
+
+STOP_WORDS_RE = r"^(i|me|my|myself|we|us|our|ours|ourselves|you|your|yours|yourself|yourselves|he|him|his|himself|" \
+                r"she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves|what|which|who|whom|whose|" \
+                r"this|that|these|those|am|is|are|was|were|be|been|being|have|has|had|having|do|does|did|doing|will|" \
+                r"would|should|can|could|ought|i'm|you're|he's|she's|it's|we're|they're|i've|you've|we've|they've|" \
+                r"i'd|you'd|he'd|she'd|we'd|they'd|i'll|you'll|he'll|she'll|we'll|they'll|isn't|aren't|wasn't|" \
+                r"weren't|hasn't|haven't|hadn't|doesn't|don't|didn't|won't|wouldn't|shan't|shouldn't|can't|cannot|" \
+                r"couldn't|mustn't|let's|that's|who's|what's|here's|there's|when's|where's|why's|how's|a|an|the|and|" \
+                r"but|if|or|because|as|until|while|of|at|by|for|with|about|against|between|into|through|during|" \
+                r"before|after|above|below|to|from|up|upon|down|in|out|on|off|over|under|again|further|then|once|" \
+                r"here|there|when|where|why|how|all|any|both|each|few|more|most|other|some|such|no|nor|not|only|own|" \
+                r"same|so|than|too|very|say|says|said|shall)$"
+
 
 class MainHandler(tornado.web.RequestHandler):
 
@@ -29,10 +45,11 @@ class MainHandler(tornado.web.RequestHandler):
 class AnalyseURLHandler(SessionMixin, tornado.websocket.WebSocketHandler):
     """
     Websocket handler
+     - to receive URL
+     - to return word frequency dictionary or error message
 
     TODO: look at reconnection strategies
     """
-
 
     # listen to any messages coming to socket
     async def on_message(self, message):
@@ -73,23 +90,56 @@ class AnalyseURLHandler(SessionMixin, tornado.websocket.WebSocketHandler):
             # remove line breaks
             words = re.sub(r"\n|\r", " ", words)
 
-            # Try to count the words with a memory efficient iterables approach.
-            # map(str.split, page_text) makes a list, items from which are chained together and then counted
-            # TODO get this working, not just making single characters
-            # word_frequency_dict = Counter(chain.from_iterable(map(methodcaller("split", " "), words)))
+            # remove stop words (leaving nouns and verbs)
+            words = re.sub(STOP_WORDS_RE, '', words)
 
-            word_frequency_dict = Counter([word for word in words.split(' ')])
-
-            print(word_frequency_dict)
-
-            with self.make_session() as session:
-
-                count = await as_future(session.query(Word).count)
-
-                print('{} words so far!'.format(count))
+            # TODO alternative if time, and ntlk stop words available
             # remove articles and prepositions [stop words]
             # stop_words = stopwords.words('english')
             # word_frequency_dict = [word_frequency_dict.pop(k, None) for k in stopwords]
 
+            word_frequency_dict = Counter([word for word in words.split(' ')])
 
-        self.write_message(json.dumps(word_frequency_dict))
+            # TODO get this working, not just making single characters
+            # Try to count the words with a memory efficient iterables approach.
+            # map(str.split, page_text) makes a list, items from which are chained together and then counted
+            # word_frequency_dict = Counter(chain.from_iterable(map(methodcaller("split", " "), words)))
+
+            # store the top 100
+            top_hundred = word_frequency_dict.most_common(100)
+
+            with self.make_session() as session:
+                words = []
+                # count = await as_future(session.query(Word).count)
+                # print('{} words so far!'.format(count))
+
+                for word, frequency in top_hundred:
+                    pk = salted_hash(word)
+                    encrypted_word = aes_encrypted(word)
+
+                    # get word from db in non-blocking fashion
+                    db_word = session.query(Word).filter_by(pk=pk).first()
+
+                    if db_word is not None:
+                        # update
+                        db_word.frequency = frequency
+                        words.append(db_word)
+                    else:
+                        # insert
+                        new_word = Word(
+                            pk=pk,
+                            word=encrypted_word,
+                            frequency=frequency)
+                        words.append(new_word)
+                    # maybe simpler way to do this with SQLAlchemy merge,
+                    # but i'm not very familiar with SQLAlchemy
+
+                    # session.merge(new_word, load=True)
+
+                session.add_all(words)
+                session.commit()
+
+            self.write_message(json.dumps(word_frequency_dict))
+        else:
+            self.write_error("No words found")
+
---
+++ url_wordcloud/utils.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+def salted_hash(text):
+    return 'buttons' + text
+
+
+def aes_encrypted(text):
+    return 'stew' + text
